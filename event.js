5086fffeea04606244031ce0bdb69d192006d7e0
amar9elbeg <89896446+amar9elbeg@users.noreply.github.com>
2021-11-02 13:22:41 -0700

    Add files via upload

---
+++ event.js
@@ -0,0 +1,22 @@
+// Exercises 1 ==> Every Time you click on Back to top button it will add 1 to count and console log the count.
+
+let count = 0;
+
+// Exercises 2 add event function to toggle header contact to show header links
+
+// Exercises 3 write function to change html5 card's text colors when click on edit button
+
+// Exercises 4 write function to change html3 card's text colors when click on edit button
+
+// Exercises 5 write function to change js card's text colors when click on edit button
+
+// Exercises 6 write function to change WiderCard card's text colors when click on edit button
+
+// Exercises 7  write function to change WiderCard card's text colors when click on edit button
+
+// Exercises 8 write function to change Github card's text colors when click on edit button
+
+// Exercises 9 write a function to add href links to to facebook and twitter and email in header
+
+// Exercises 10  write a function to change the order of the cards: <== button will put the first card at the end
+//==> will do the reverse
