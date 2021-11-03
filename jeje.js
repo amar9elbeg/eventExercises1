1d9c72b8de7d6949dd892fffd18b97d2fdceb038
Enkhjinjeje <ekhjinjeje@gmail.com>
2021-11-02 18:06:56 -0700

    did-class-work

---
+++ jeje.js
@@ -0,0 +1,10 @@
+console.log("hi")
+function hello(){
+    let uuugna = document.querySelector("header") ;
+    if (uuugna.innerHTML === "jeje"){
+        uugna.innerHTML === "jesje"
+    }else{
+        uuugna.innerHTML === "jeje"
+    }
+    
+}
