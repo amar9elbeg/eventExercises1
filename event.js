2f8cd8d3232dfa369e828f9dcde02b1711a013e4
Uuganbaatar Myagmarjav <umigmarjav0@gmail.com>
2021-11-02 21:06:14 -0400

    Uugna's classwork

--- event.js
+++ event.js
@@ -1,10 +1,35 @@
 // Exercises 1 ==> Every Time you click on Back to top button it will add 1 to count and console log the count.
 
 let count = 0;
-
+// function number() {
+//     count += 1
+//     console.log(count)
+// }
+function footerClick() {
+  const footer = document.querySelector("footer");
+  footer.addEventListener("click", () => {
+    count += 1;
+    console.log(count);
+  });
+}
+footerClick();
 // Exercises 2 add event function to toggle header contact to show header links
 
 // Exercises 3 write function to change html5 card's text colors when click on edit button
+
+function colorChanger() {
+  const edit = document.querySelector(".album").querySelector(".row").querySelector(".btn-group").children[1];
+  const card = document.querySelector(".album").querySelector(".row").querySelector(".card-text");
+  
+edit.addEventListener("click", () => {
+    if (card.style.color === "black") {
+      card.style.color = "red";
+    } else {
+      card.style.color = "black";
+    }
+  });
+}
+colorChanger();
 
 // Exercises 4 write function to change html3 card's text colors when click on edit button
 
